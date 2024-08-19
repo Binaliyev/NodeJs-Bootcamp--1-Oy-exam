4eda62a1d9562c46e51ef53d8d7cfed38b79cd7d
Binaliyev <bnaliyevbinali@gmail.com>
2024-08-19 12:18:19 -0700

    NodeJs(Bootcamp)-1-Oy-exam

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 // Bu script fileda moviesni JS codelari yoziladi 
 const elList = document.querySelector(".js-movie-card-list");
 const shadowElementTemplate = document.querySelector("template").content;
-function render_movie(json) {
-    json = json.slice(0, 50);
+function renderMovie(json) {
+    json = json.slice(0, 20);
     const docFragment = document.createDocumentFragment();
     json.forEach(function (item) {
         let clone = shadowElementTemplate.cloneNode(true);
-        let movie_img = clone.querySelector(".js-movie-picture");
-        movie_img.src = item.poster;
-        let movie_title = clone.querySelector(".js-movie-title");
-        movie_title.textContent = item.title;
-        let movie_rating = clone.querySelector(".js-movie-rating");
-        movie_rating.textContent = Math.round(Math.random() * 10);
-        let movie_year = clone.querySelector(".js-movie-year");
-        movie_year.textContent = item.release_date;
-        let movie_desc = clone.querySelector(".js-movie-desc");
-        movie_desc.textContent = item.overview;
-        let movie_genres = clone.querySelector(".js-movie-genres");
-        movie_genres.textContent =`Movie genres: ${item.genres.join(", ")}`
+        let movieImg = clone.querySelector(".js-movie-picture");
+        movieImg.src = item.poster;
+        let movieTitle = clone.querySelector(".js-movie-title");
+        movieTitle.textContent = item.title;
+        let movieRating = clone.querySelector(".js-movie-rating");
+        movieRating.textContent = Math.round(Math.random() * 10);
+        let movieYear = clone.querySelector(".js-movie-year");
+        movieYear.textContent = item.release_date;
+        let movieDesc = clone.querySelector(".js-movie-desc");
+        movieDesc.textContent = item.overview;
+        let movieGenres = clone.querySelector(".js-movie-genres");
+        movieGenres.textContent = `Movie genres: ${item.genres.join(", ")}`
         docFragment.append(clone);
     })
     elList.append(docFragment);
 }
-render_movie(films);
+renderMovie(films);
--- tasks.js
+++ tasks.js
@@ -6,8 +6,7 @@
 
 // const numbers = [23, 45, 67, 89, 34, 56, 33, 151, 78, 20];
 // let maxNum = 0;
-// for (let i = 0; i < numbers.length; i++) {
-//     const nums = numbers[i];
+// for (let nums of numbers) {
 //     if(maxNum < nums) maxNum = nums;
 // }
 // console.log(maxNum);
@@ -17,45 +16,49 @@
 // Arraydagi eng uzun so'zni topib uning oxirgisidan bitta oldingi harfini topish
 
 // const fruites = ["Olma", "Nok", "Anor", "Chegara"];
-// let maxLen;
+// let maxLength;
 // for (const key in fruites) {
-//     let fruites_value_length = fruites[key].length
-//     if(Math.max(fruites_value_length) == fruites_value_length) maxLen=key;
+//     let fruitesValueLength = fruites[key].length;
+//     if(Math.max(fruitesValueLength) == fruitesValueLength) maxLength=key;
 // }
-// let re = fruites[Number(maxLen)].at(-2)
-// console.log(re);
+// let result = fruites[Number(maxLength)].at(-2);
+// console.log(result);
 
 // Masala - 3
 
 // Arraydagi barcha stringning lengthini topish
 
 // const fruites = ["Olma", "anor", "Nok"];
-// let re = fruites.join("").length;
-// let re = fruites.map(function(item){return item.length});
-// console.log(re);
+// ------------------
+// let result = [];
+// result.push(fruites.join("").length);
+// ------------------ 
+// let result = fruites.map(function(item){return item.length});
+// console.log(result);
 
 // Masala - 4
 
 // Arrayda kamida 6 ta toq va juft son bor, va shundan juft sonlarning yarmini ya'ni 18 juft soni bo'lsa uning yarmi 9 sonini bir arrayga solib qaytarib bersin ya'ni hamma juftlarning yarmini bitta arrayga joylash kerak
 
 // const numbers = [23, 58, 96, 31, 74, 44, 152, 531, 26];
-// let re = numbers.filter(function(item){return !(item%2)}).map(function(item){return item/2});
-// console.log(re);
+// let result = [];
+// numbers.forEach(function(nums){if(nums%2==0) result.push(nums/2);});
+// console.log(result);
 
 // Masala - 5
 
 // Arraydagi katta harf bilan boshlangan so'zlarning birinchi harfini kichik qolgan harfini katta qilib, kichik harf bilan boshlanganlarini birinchi harfini katta qolganlarini kichkina qib bersin
 
 // const fruites = ["olma", "o'rik", "Nok", "avacado"];
-// let re = "";
-// for (const v of fruites) {
-//     if (v.at(0) == v.at(0).toUpperCase()) re+=`${v.at(0).toLowerCase()}${v.slice(1, v.length).toUpperCase()}, `;
-//     if(v.at(0) == v.at(0).toLowerCase()) re+=`${v.at(0).toUpperCase()}${v.slice(1, v.length)}, `;
+// let result = [];
+// for (const fruitesValue of fruites) {
+//     let fruitesValueToLowerCase = fruitesValue.at(0).toLowerCase();
+//     let fruitesValueToUpperCase = fruitesValue.at(0).toUpperCase();
+//     let fruitesValueSlice = fruitesValue.slice(1, fruitesValue.length);
+//     if (fruitesValue.at(0) == fruitesValueToUpperCase) result.push(fruitesValueToLowerCase+fruitesValueSlice.toUpperCase())
+//     else result.push(fruitesValueToUpperCase+fruitesValueSlice);
 // }
-// console.log(re);
-
-
-
+// console.log(result);
 
 // Masala - 6
 
@@ -64,16 +67,17 @@
 // example: olma => olma olma olma
 
 // let str = prompt("str kiriting");
-// let re = ""
-// for (let i = 0; i < 3; i++) {re+=`${str} `}
-// console.log(re);
+// let result = ""
+// for (let i = 0; i < 3; i++) {re+=str}
+// console.log(result);
 
 // Masala - 7
 // Ichma ich array bor va shu arrayning juft arraylarning birinchi elementining birinchi harflarini birlashtirib qaytarib bersin
 
 // const fruitesArray = [["Olma"], ["Bexi"], ["Anor"], ["Xurmo"], ["GIlos"], ["Kiwi"], ["Banan"],];
-// let re = fruitesArray.filter(function(item){return item[0].length % 2 == 0}).map(function(item){return item[0][0]}).join("");
-// console.log(re);
+// let result = "";
+// fruitesArray.forEach(function(fruites){if(fruites[0].length % 2 == 0)result+=fruites[0].at(0);});
+// console.log(result);
 
 // example: const fruitesArray = [["Olma"],["Bexi"],["Anor"],["Xurmo"],["GIlos"],["Kiwi"],["Banan"],];  => BXK
 
